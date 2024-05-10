e291771e03a5efe1ee5ce22778a367128177b688
Swaran Singh <107933244+ADCByDefault@users.noreply.github.com>
2024-05-10 02:54:40 +0200

    change error handeling, created database

--- src/authentication/connessione.php
+++ src/authentication/connessione.php
@@ -1,11 +1,12 @@
 <?php
-$dbHost="";
-$dbUsername="root";
-$dbPassword="";
-$dbName="ilgirone";
-$conn = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
-if($conn->connect_error){
-    header("Location: ");
-    die();
+session_start();
+$dbHost = "";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "ilgirone";
+$conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+if ($conn->connect_error) {
+    $_SESSION["error"] = "Internal server error";
+    header("Location: error/error500.php");
+    exit();
 }
-?>
--- src/error/error400.php
+++ src/error/error400.php
@@ -1,4 +1,9 @@
 <?php
 session_start();
-$_SESSION["error"] = "Errore 400";
-?>
+if (isset($_SESSION["error"])) {
+    header("Location: index.php");
+} else {
+    $_SESSION["error"] = "Bad request";
+    header("Location: index.php");
+}
+die();
